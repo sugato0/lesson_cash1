f43bfab0e87504abcc167d05c6f046548933b109
dssaharov <sugatokavary@gmail.com>
2025-12-17 17:29:44 +0200

    Initial commit

---
+++ 17.12.25.py
@@ -0,0 +1,123 @@
+# hello world 123423
+# name = 123
+# str - строки
+# a = "hello world"
+# print(a)
+# int - целочисленный тип
+# b = 1234
+#type - тип данных проверка
+# print(type(b),b)
+#float - число с плавающей точкой
+# a = .1
+# print(type(a),a)
+#bool - логический тип данных
+# a = True #False
+# print(type(a),a)
+# ctrl /
+#list - список,
+#набор элементов не связанных по типу
+# a = [1,2,"hello",True,9,3]
+# a = [1,2,3,4,5,6,7,8]
+# print(a[0]) #1
+#tuple - набор неизмяимых элементов
+# a = (1,2)
+# print(type(a),a)
+#set - набор уникальных элементов
+# a = {1,2,3,4,5,5,5}
+# print(a[0])
+#dict - словарь
+# a = {"key":"value","key1":"value1"}
+# print(a["key"])
+
+# type()
+# id - код памяти
+#= - присваивание
+# a = 10
+# b = a
+# b = 20
+# a = 20
+
+# a = 40
+# b = 20
+# print(a + b)
+# a = [1,2]+[3]
+# print(a)
+# print([1,2]+[3])
+# print(a * b)
+# print([0] * 10)
+# print("hello" * 10)
+# print(a / b)
+# print(a - b)
+
+
+#возведение в степень
+# print(a ** b)
+# a = 10 ** 0.5
+#цулочисленное деление
+# print(a // b)
+#остаток от деления
+# print(a % b)
+
+# > < == != <= >=
+# операции сравнивают
+# 2 одинаковых типа данных
+#True/False
+# print(10 <= 10, 10 < 10)
+
+# or and not #всегда True/False
+# a = True
+# b = False
+# 0 0 0
+# 0 1 1
+# 1 0 1
+# 1 1 1
+# print(a or b)
+# 0 0 0
+# 0 1 0
+# 1 0 0
+# 1 1 1
+# print(a and b)
+# print(not 10 < 20)
+# a = False
+# b = True
+# 0 0 1
+# 0 1 0
+# 1 0 0
+# 1 1 0
+# print(not (a or b)) #Стрелка пирса
+# ^
+# 0 0 0
+# 0 1 1
+# 1 0 1
+# 1 1 0
+# print(a ^ b)
+# print((a == True and b == False) or
+#       (a == False and b == True))
+# ->
+# 0 0 1
+# 0 1 1
+# 1 0 0
+# 1 1 1
+# print(a <= b) #импликация
+
+# import math
+# print(math.pi)
+# from math import pi
+# print(pi)
+# import math as m
+# print(m.pi)
+# from math import *
+# print(pi,e,sqrt(25))
+
+# a = "12"
+# print(int(a) + 30)
+# a = "12.3"
+# print(float(a))
+# a = [1,2,3,3,4,5,6,3,2]
+# print(set(a))
+
+# a = "3f" #2
+# print(int(a,16))
+
+#o - восмеричная, b - двоичная, x - 16- ричная
+# print(format(20,'b'))
